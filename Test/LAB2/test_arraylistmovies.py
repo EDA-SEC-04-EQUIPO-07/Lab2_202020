ca9740e6e05f7e50ef91781bde3ddd78782e3753
jessiRob <69654419+jessiRob@users.noreply.github.com>
2020-08-23 10:16:37 -0500

    Update test_arraylistmovies.py

--- Test/LAB2/test_arraylistmovies.py
+++ Test/LAB2/test_arraylistmovies.py
@@ -15,7 +15,7 @@ def cmpfunction (element1, element2):
 
 
 @pytest.fixture
-def lst_c ():
+def lst ():
     lst = slt.newList(cmpfunction)
     return lst
 
@@ -30,10 +30,8 @@ def movies ():
     movies.append({'id': '5', 'actor1_name': 'none', 'actor1_gender': '0', 'actor2_name': 'none', 'actor2_gender': '0', 'actor3_name': 'none', 'actor3_gender': '0', 'actor4_name': 'none', 'actor4_gender': '0', 'actor5_name': 'none', 'actor5_gender': '0', 'actor_number': '0', 'director_name': 'Timo Novotny', 'director_gender': '0', 'director_number': '1', 'producer_name': 'Timo Novotny', 'producer_number': '2', 'screeplay_name': 'Michael Glawogger', 'editor_name': 'Timo Novotny'})
     return movies
 
-
-
 @pytest.fixture
-def lst(movies):
+def lstmovies(movies):
     lst = slt.newList(cmpfunction)
     for i in range(0,5):    
         slt.addLast(lst,movies[i])    
@@ -75,77 +73,77 @@ def test_addLast (lst, movies):
 
 
 
-def test_getElement(lst, movies):
-    movie = slt.getElement(lst, 1)
+def test_getElement(lstmovies, movies):
+    movie = slt.getElement(lstmovies, 1)
     assert book == movies[0]
-    movie = slt.getElement(lst, 5)
+    movie = slt.getElement(lstmovies, 5)
     assert movie == movies[4]
 
 
 
-def test_removeFirst (lst, movies):
-    assert slt.size(lst) == 5
-    slt.removeFirst(lst)
-    assert slt.size(lst) == 4
-    movie = slt.getElement(lst, 1)
+def test_removeFirst (lstmovies, movies):
+    assert slt.size(lstmovies) == 5
+    slt.removeFirst(lstmovies)
+    assert slt.size(lstmovies) == 4
+    movie = slt.getElement(lstmovies, 1)
     assert movie  == movies[1]
 
 
 
-def test_removeLast (lst, movies):
-    assert slt.size(lst) == 5
-    slt.removeLast(lst)
-    assert slt.size(lst) == 4
-    movie = slt.getElement(lst, 4)
+def test_removeLast (lstmovies, movies):
+    assert slt.size(lstmovies) == 5
+    slt.removeLast(lstmovies)
+    assert slt.size(lstmovies) == 4
+    movie = slt.getElement(lstmovies, 4)
     assert movie  == movies[3]
 
 
 
-def test_insertElement (lst_c, movies):
-    assert slt.isEmpty(lst_c) is True
-    assert slt.size(lst_c) == 0
-    slt.insertElement (lst_c, movies[0], 1)
-    assert slt.size(lst_c) == 1
-    slt.insertElement (lst_c, movies[1], 2)
-    assert slt.size(lst_c) == 2
-    slt.insertElement (lst_c, movies[2], 1)
-    assert slt.size(lst_c) == 3
-    movie = slt.getElement(lst_c, 1)
+def test_insertElement (lst, movies):
+    assert slt.isEmpty(lst) is True
+    assert slt.size(lst) == 0
+    slt.insertElement (lst, movies[0], 1)
+    assert slt.size(lst) == 1
+    slt.insertElement (lst, movies[1], 2)
+    assert slt.size(lst) == 2
+    slt.insertElement (lst, movies[2], 1)
+    assert slt.size(lst) == 3
+    movie = slt.getElement(lst, 1)
     assert movie == movies[2]
-    movie = slt.getElement(lst_c, 2)
+    movie = slt.getElement(lst, 2)
     assert movie == movies[0]
 
 
 
-def test_isPresent (lst, movies):
+def test_isPresent (lstmovies, movies):
     movie = {'id': '8', 'actor1_name': 'none', 'actor1_gender': '0', 'actor2_name': 'none', 'actor2_gender': '0','actor3_name': 'none', 'actor3_gender': '0', 'actor4_name': 'none', 'actor4_gender': '0', 'actor5_name': 'none', 'actor5_gender': '0', 'actor_number': '0', 'director_name': 'Timo Novotny', 'director_gender': '0', 'director_number': '1', 'producer_name': 'Timo Novotny', 'producer_number': '2', 'screeplay_name': 'Michael Glawogger', 'editor_name': 'Timo Novotny'}
-    print(slt.isPresent (lst, movies[2]))
-    assert slt.isPresent (lst, movies[2]) > 0
-    assert slt.isPresent (lst, movie) == 0
+    print(slt.isPresent (lstmovies, movies[2]))
+    assert slt.isPresent (lstmovies, movies[2]) > 0
+    assert slt.isPresent (lstmovies, movie) == 0
     
 
 
-def test_deleteElement (lst, movies):
-    pos = slt.isPresent (lst, movies[2])
+def test_deleteElement (lstmovies, movies):
+    pos = slt.isPresent (lstmovies, movies[2])
     assert pos > 0
-    movie = slt.getElement(lst, pos)
+    movie = slt.getElement(lstmovies, pos)
     assert movie == movies[2]
-    slt.deleteElement (lst, pos)
-    assert slt.size(lst) == 4
-    movie = slt.getElement(lst, pos)
+    slt.deleteElement (lstmovies, pos)
+    assert slt.size(lstmovies) == 4
+    movie = slt.getElement(lstmovies, pos)
     assert movie == movies[3]
 
 
-def test_changeInfo (lst):
+def test_changeInfo (lstmovies):
     movie10 = {'id': '8', 'actor1_name': 'none', 'actor1_gender': '0', 'actor2_name': 'none', 'actor2_gender': '0','actor3_name': 'none', 'actor3_gender': '0', 'actor4_name': 'none', 'actor4_gender': '0', 'actor5_name': 'none', 'actor5_gender': '0', 'actor_number': '0', 'director_name': 'Timo Novotny', 'director_gender': '0', 'director_number': '1', 'producer_name': 'Timo Novotny', 'producer_number': '2', 'screeplay_name': 'Michael Glawogger', 'editor_name': 'Timo Novotny'}
-    slt.changeInfo (lst, 1, movie10)
-    movie = slt.getElement(lst, 1)
+    slt.changeInfo (lstmovies, 1, movie10)
+    movie = slt.getElement(lstmovies, 1)
     assert movie10 == movie
 
 
-def test_exchange (lst, movies):
-    book1 = slt.getElement(lst, 1)
-    book5 = slt.getElement(lst, 5)
-    slt.exchange (lst, 1, 5)
-    assert slt.getElement(lst, 1) == book5
-    assert slt.getElement(lst, 5) == book1
+def test_exchange (lstmovies, movies):
+    book1 = slt.getElement(lstmovies, 1)
+    book5 = slt.getElement(lstmovies, 5)
+    slt.exchange (lstmovies, 1, 5)
+    assert slt.getElement(lstmovies, 1) == book5
+    assert slt.getElement(lstmovies, 5) == book1
