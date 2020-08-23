312f40cb6c4a341b6298b812c884112d5c9fe74d
MartinUbaque <69693663+MartinUbaque@users.noreply.github.com>
2020-08-23 16:30:03 -0500

    Update insertionsort.py

--- Sorting/insertionsort.py
+++ Sorting/insertionsort.py
@@ -23,7 +23,18 @@
 import config as cf
 from ADT import list as lt
 
-def insertionSort (lst, lessfunction): 
+#MARTIN
+def insertionSort (lst, lessfunction, criteria): 
+    size =  lt.size(lst) 
+    pos1 = 1
+    while pos1 <= size:
+        pos2 = pos1
+        while (pos2 >1) and (lessfunction (lt.getElement(lst, pos2),lt.getElement(lst, pos2-1),criteria)):
+            lt.exchange (lst, pos2, pos2-1)
+            pos2 -= 1
+        pos1 += 1
+        
+"""def insertionSort (lst, lessfunction): 
     size =  lt.size(lst) 
     pos1 = 1
     while pos1 <= size:
@@ -31,4 +42,4 @@ def insertionSort (lst, lessfunction):
         while (pos2 >1) and (lessfunction (lt.getElement(lst, pos2),lt.getElement(lst, pos2-1))):
             lt.exchange (lst, pos2, pos2-1)
             pos2 -= 1
-        pos1 += 1
+        pos1 += 1"""
