8cdee6dca867a9b4c946781dbf81148eba65c43e
Maria Paula <mau@MacBook-Air-de-Maria-2.local>
2020-08-23 18:14:48 -0500

    test

--- App/app.py
+++ App/app.py
@@ -32,6 +32,9 @@ import csv
 from ADT import list as lt
 from DataStructures import listiterator as it
 from DataStructures import liststructure as lt
+from Sorting import insertionsort as ins
+from Sorting import selectionsort as sel
+from Sorting import shellsort as she
 
 from time import process_time 
 
@@ -76,6 +79,7 @@ def printMenu():
     print("2- Contar los elementos de la Lista")
     print("3- Contar elementos filtrados por palabra clave")
     print("4- Consultar elementos a partir de dos listas")
+    print("5- Ordenar elementos")
     print("0- Salir")
 
 def countElementsFilteredByColumn(criteria, column, lst):
@@ -107,52 +111,115 @@ def countElementsFilteredByColumn(criteria, column, lst):
         print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
     return counter
 
-def countElementsByCriteria(criteria, column, lst):
+def countElementsByCriteria(criteria, lst1,lst2):
     """
     Retorna la cantidad de elementos que cumplen con un criterio para una columna dada
     """
-    return 0
+    lst = lt.newList()
+    promedio=0
+    ids=[]
 
+    iterator = it.newIterator(lst1)
+    while  it.hasNext(iterator):
+        element = it.next(iterator)
+        if criteria.lower() in element["director_name"].lower(): 
+            lt.addLast(lst,element)
+            ids.append(element["id"])
+    
+    iterator = it.newIterator(lst2)
+    while  it.hasNext(iterator):
+        pelicula = it.next(iterator)
+        if pelicula["id"] in ids: 
+            promedio+=float(pelicula["vote_average"])
+    promedio/=lst["size"]
+    
+    return (lst,lst["size"],promedio)
+
+def less(element1, element2, criteria):
+    if float(element1[criteria]) < float(element2[criteria]):
+        return True
+    return False
+
+def greater (element1,element2, criteria):
+    if float(element1[criteria]) > float(element2[criteria]):
+        return True
+    return False
+   
+   
 def orderElementsByCriteria(function, column, lst, elements):
     """
     Retorna una lista con cierta cantidad de elementos ordenados por el criterio
     """
-    return 0
+    if column == "1":
+        column="vote_count"
+    elif column == "2":
+        column="vote_average"
+    else:
+        print("Valor no valido para criterio de busqueda")
+    lista=[]
+    if function.lower()=="crecimiento":
+        ins.insertionSort(lst,greater,column)
+    elif function.lower()=="decrecimiento":
+        ins.insertionSort(lst,less,column)
+    for i in range(0,int(elements)):
+        lista.append(lt.getElement(lst, i)["original_title"])
+    return lista
 
 def main():
     """
     Método principal del programa, se encarga de manejar todos los metodos adicionales creados
-
     Instancia una lista vacia en la cual se guardarán los datos cargados desde el archivo
     Args: None
     Return: None 
     """
-    lista = lt.newList()   # se require usar lista definida
+    lista_casting = lt.newList()   # se require usar lista definida
+    lista_details = lt.newList()
     while True:
         printMenu() #imprimir el menu de opciones en consola
         inputs =input('Seleccione una opción para continuar\n') #leer opción ingresada
         if len(inputs)>0:
             if int(inputs[0])==1: #opcion 1
-                lista = loadCSVFile("Data/test.csv") #llamar funcion cargar datos
-                print("Datos cargados, ",lista['size']," elementos cargados")
+                lista_casting = loadCSVFile("Data/MoviesCastingRaw-small.csv") #llamar funcion cargar datos
+                lista_details = loadCSVFile("Data/SmallMoviesDetailsCleaned.csv")
+                print("Datos cargados en lista casting, ",lista_casting['size']," elementos cargados")
+                print("Datos cargados en lista details, ",lista_details['size']," elementos cargados")
             elif int(inputs[0])==2: #opcion 2
-                if lista==None or lista['size']==0: #obtener la longitud de la lista
-                    print("La lista esta vacía")    
-                else: print("La lista tiene ",lista['size']," elementos")
+                if lista_details==None or lista_details['size']==0: #obtener la longitud de la lista
+                    print("La lista details esta vacía")  
+                elif lista_casting==None or lista_casting['size']==0: #obtener la longitud de la lista
+                    print("La lista casting esta vacía")    
+                else: print("La lista details tiene ",lista_details['size']," elementos y la lista casting tiene ",lista_casting['size']," elementos" )
             elif int(inputs[0])==3: #opcion 3
-                if lista==None or lista['size']==0: #obtener la longitud de la lista
-                    print("La lista esta vacía")
+                if lista_details==None or lista_details['size']==0: #obtener la longitud de la lista
+                    print("La lista details esta vacía")  
+                elif lista_casting==None or lista_casting['size']==0: #obtener la longitud de la lista
+                    print("La lista casting esta vacía")  
                 else:   
                     criteria =input('Ingrese el criterio de búsqueda\n')
-                    counter=countElementsFilteredByColumn(criteria, "nombre", lista) #filtrar una columna por criterio  
+                    counter=countElementsFilteredByColumn(criteria, "director_name", lista_casting) #filtrar una columna por criterio  
                     print("Coinciden ",counter," elementos con el crtierio: ", criteria  )
             elif int(inputs[0])==4: #opcion 4
-                if lista==None or lista['size']==0: #obtener la longitud de la lista
-                    print("La lista esta vacía")
+                if lista_details==None or lista_details['size']==0: #obtener la longitud de la lista
+                    print("La lista details esta vacía")  
+                elif lista_casting==None or lista_casting['size']==0: #obtener la longitud de la lista
+                    print("La lista casting esta vacía")
                 else:
-                    criteria =input('Ingrese el criterio de búsqueda\n')
-                    counter=countElementsByCriteria(criteria,0,lista)
-                    print("Coinciden ",counter," elementos con el crtierio: '", criteria ,"' (en construcción ...)")
+                    criteria =input('Ingrese el nombre del director\n')
+                    print(countElementsByCriteria(criteria,lista_casting,lista_details))
+                    lista,counter,promedio=countElementsByCriteria(criteria,lista_casting,lista_details)
+                    print("Las peliculas dirigidas por " + criteria +  " son "+ str(lista))
+                    print ("Hay "+str(counter)+" películas buenas de ese director. Y "+str(promedio)+" es su promedio de la votacion.") 
+            elif int(inputs[0])==5: #opcion 5
+                if lista_details==None or lista_details['size']==0: #obtener la longitud de la lista
+                    print("La lista details esta vacía")  
+                elif lista_casting==None or lista_casting['size']==0: #obtener la longitud de la lista
+                    print("La lista casting esta vacía")
+                else:
+                    criteria =input('Ingrese 1 si el criterio de busqueda es COUNT o ingrese 2 si es AVERAGE\n')
+                    crecimiento =input("¿Quiere la lista en crecimiento o decrecimiento?\n")
+                    tamaño =input("Ingrese cuantas posiciones quiere que tenga su lista\n")
+                    lista=orderElementsByCriteria(crecimiento,criteria,lista_details,tamaño)
+                    print ("La lista de peliculas solicitada es:",lista)
             elif int(inputs[0])==0: #opcion 0, salir
                 sys.exit(0)
                 
